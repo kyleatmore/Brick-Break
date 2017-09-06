3e64fac5abd6461664296490cd2d03b3f711f8df
Kyle Atmore <atmorek@gmail.com>
2017-09-06 15:57:22 -0400

    Setup webpack and entry file

---
+++ breakout.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const board = document.getElementById('board');
+  board.height = 300;
+  board.width = 300;
+});
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+var path = require('path');
+var webpack = require("webpack");
+
+var plugins = [];
+var devPlugins = [];
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
+
+module.exports = {
+  entry: './breakout.js',
+  output: {
+    filename: './lib/bundle.js',
+  },
+  plugins: plugins,
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
