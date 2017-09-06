5a0f9638731b0c0b149cf5278d7989e4b94d0c69
Kyle Atmore <atmorek@gmail.com>
2017-09-06 19:03:55 -0400

    Add keydown event listener to move paddle

--- breakout.js
+++ breakout.js
@@ -7,4 +7,19 @@ document.addEventListener('DOMContentLoaded', () => {
   const ctx = canvas.getContext('2d');
 
   const game = new Game(ctx);
+  const paddle = game.paddle;
+  window.paddle = paddle;
+  window.bricks = game.bricks;
+  window.ctx = ctx;
+
+  document.addEventListener("keydown", (event) => {
+    switch(event.keyCode) {
+      case 39:
+        paddle.move(20);
+        break;
+      case 37:
+        paddle.move(-20);
+        break;
+    }
+  });
 });
