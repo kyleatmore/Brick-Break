a26527c802afdf1484067ffcb8cfc42f5fa8b076
Kyle Atmore <atmorek@gmail.com>
2017-09-06 23:42:45 -0400

    Create brick elements from images

--- breakout.js
+++ breakout.js
@@ -1,25 +1,29 @@
 import Game from './lib/game';
+import Brick from './lib/brick';
 
 document.addEventListener('DOMContentLoaded', () => {
   const canvas = document.getElementById('board');
   canvas.height = 600;
-  canvas.width = 670;
+  canvas.width = 700;
   const ctx = canvas.getContext('2d');
 
   const game = new Game(ctx);
-  const paddle = game.paddle;
-  window.paddle = paddle;
+  // const paddle = game.paddle;
+  // window.paddle = paddle;
   window.bricks = game.bricks;
-  window.ctx = ctx;
-
-  document.addEventListener("keydown", (event) => {
-    switch(event.keyCode) {
-      case 39:
-        paddle.move(20);
-        break;
-      case 37:
-        paddle.move(-20);
-        break;
-    }
-  });
+  // window.ctx = ctx;
+  // window.ball = game.ball;
+  //
+  // document.addEventListener("keydown", (event) => {
+  //   switch(event.keyCode) {
+  //     case 39:
+  //       paddle.move(10);
+  //       break;
+  //     case 37:
+  //       paddle.move(-10);
+  //       break;
+  //   }
+  // });
+  //
+  // setInterval(() => { game.ball.move(); }, 200);
 });
